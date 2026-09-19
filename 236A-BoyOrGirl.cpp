21de51e85531e594ec544caf73aef4d7de03f6de
Versh-03 <25113cn167@glbitm.ac.in>
2026-09-19 16:00:17 +0530

    Create 236A-BoyOrGirl.cpp

---
+++ 236A-BoyOrGirl.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    string str1;
+    unordered_set<char> st;
+    cin>>str1;
+    for(char x: str1){
+        st.insert(x);
+    }
+    cout<<(st.size()%2==0?"CHAT WITH HER!":"IGNORE HIM!");
+    return 0;
+}
